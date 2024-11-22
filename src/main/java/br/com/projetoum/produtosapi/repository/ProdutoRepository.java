1768a7009de7b80b3c1b305b9711a1090f7d737c
Leonardo Trindade <leotrindadeltth@outlook.com>
2024-11-22 15:23:51 -0300

    first commit

---
+++ src/main/java/br/com/projetoum/produtosapi/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package br.com.projetoum.produtosapi.repository;
+
+import br.com.projetoum.produtosapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
+}
